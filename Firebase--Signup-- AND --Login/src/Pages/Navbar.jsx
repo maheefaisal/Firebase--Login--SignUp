05fd61f081044393e92225cc870a75058a393929
Ehfaz Mahee <mahee504@gmail.com>
2023-07-29 10:30:44 +0600

    added readme file contentds and fixed the look of website

--- Firebase--Signup-- AND --Login/src/Pages/Navbar.jsx
+++ Firebase--Signup-- AND --Login/src/Pages/Navbar.jsx
@@ -1,79 +1,3 @@
-// import { useContext } from "react";
-// import { Link } from "react-router-dom";
-// import { AuthContext } from "./AuthProvider";
-
-// const Navbar = () => {
-//   const { user, logOut } = useContext(AuthContext);
-
-//   console.log(user);
-//   const handleLogOut = () => {
-//     logOut()
-//       // eslint-disable-next-line no-unused-vars
-//       .then((result) => {})
-//       // eslint-disable-next-line no-unused-vars
-//       .catch((err) => {});
-//   };
-
-//   return (
-//     <>
-//       <div className="navbar bg-base-300">
-//         <div className="navbar-start">
-//           <div className="dropdown">
-//             <label tabIndex={0} className="btn btn-ghost lg:hidden">
-//               <svg
-//                 xmlns="http://www.w3.org/2000/svg"
-//                 className="h-5 w-5"
-//                 fill="none"
-//                 viewBox="0 0 24 24"
-//                 stroke="currentColor"
-//               >
-//                 <path
-//                   strokeLinecap="round"
-//                   strokeLinejoin="round"
-//                   strokeWidth="2"
-//                   d="M4 6h16M4 12h8m-8 6h16"
-//                 />
-//               </svg>
-//             </label>
-
-//           </div>
-//           <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-//         </div>
-//         <div className="navbar-center hidden lg:flex">
-//           <ul className="menu menu-horizontal px-1">
-//             <button className="btn btn-accent">
-//               <Link to="/login">Login</Link>
-//             </button>
-
-//             <button className="btn btn-accent">
-//               <Link to="/signup">SignUp</Link>
-//             </button>
-//           </ul>
-//         </div>
-
-//         {user && (
-//           <span className="ml-12">
-//             <span>Email:</span> {user.email}
-//              <span>Name</span> {user.displayName}
-//             <div className="avatar mx-5">
-//               <div className="w-20 rounded-full">
-//                 <img src={user.photoURL} alt="" />
-//               </div>
-//             </div>
-
-//             <button onClick={handleLogOut}  className="btn btn-neutral">LogOut</button>
-
-//             {/* <div onClick={handleLogOut} className="navbar-end">
-//               LogOut
-//             </div> */}
-//           </span>
-//         )}
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Navbar;
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "./AuthProvider";
@@ -91,36 +15,37 @@ const Navbar = () => {
   return (
     <>
       <div className="navbar bg-base-300">
-        <div className="navbar-start">{/* ... */}</div>
-        <div className="navbar-center hidden lg:flex">
+        <div className="navbar-start flex items-center space-x-4">
           <ul className="menu menu-horizontal px-1">
             <button className="btn btn-accent">
               <Link to="/login">Login</Link>
             </button>
-
             <button className="btn btn-accent">
               <Link to="/signup">SignUp</Link>
             </button>
           </ul>
         </div>
 
+   
+
         {user && (
-          <span className="ml-12">
-            <p className="text-xs">
-              <span>Email:</span> {user.email}
-            </p>
-            <p className="mt-2">
-              {" "}
-              {/* Add margin-top here */}
-              <span>Name:</span> {user.displayName}
-            </p>
+          <span className="ml-auto flex items-center space-x-4">
+            <div className="flex flex-col">
+              <p className="text-xs">
+                <span className="font-bold">Email:</span> {user.email}
+              </p>
+              <p className="mt-2">
+                <span className="font-bold">Name:</span> {user.displayName}
+              </p>
+            </div>
+
             <div className="avatar mx-5">
               <div className="w-20 rounded-full">
                 <img src={user.photoURL} alt="" />
               </div>
             </div>
 
-            <button onClick={handleLogOut} className="btn btn-neutral">
+            <button className="btn btn-neutral" onClick={handleLogOut}>
               LogOut
             </button>
           </span>
--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-"# Firebase--Login--SignUp" 
+## Firebase Login & SignUp
+
+![Project Preview](project-preview.png) <!-- Replace "project-preview.png" with an actual preview image if available -->
+
+This is a professional-grade web application that demonstrates the implementation of user authentication using Firebase. Users can sign up and log in securely to access the application's features.
+
+### Live Demo
+
+Check out the live demo of the application [here](https://dapper-elf-fe7479.netlify.app/).
+
+### Technologies Used
+
+- React
+- Tailwind CSS
+- React Router Dom
+
+### Features
+
+- User registration and authentication using Firebase
+- Secure password hashing and storage
+- Responsive and user-friendly interface
+- Smooth navigation with React Router Dom
+
+### How to Run the Project Locally
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/maheefaisal/Firebase--Login--SignUp.git
+```
+
+2. Install the dependencies:
+
+```bash
+cd Firebase--Login--SignUp
+npm install
+```
+
+3. Start the development server:
+
+```bash
+npm start
+```
+
+
+
+### Getting Help
+
+If you encounter any issues or have questions regarding the project, please open an issue in the [GitHub repository](https://github.com/maheefaisal/Firebase--Login--SignUp/issues).
+
+### Contribution
+
+Contributions are welcome! If you'd like to improve the project or add new features, feel free to create a pull request.
+
+### License
+
+This project is licensed under the [MIT License](LICENSE).
+
+---
+
+By [Ehfaz Mahee](https://github.com/maheefaisal) <!-- Replace "Ehfaz Mahee" and "https://github.com/maheefaisal" with your actual name and GitHub profile link -->
